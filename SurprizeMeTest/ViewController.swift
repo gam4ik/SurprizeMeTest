8d64f7064b05d86d7840dec0aa725084e60337aa
Gam4ik <gambid2006@gmail.com>
2020-06-09 13:09:25 +0300

    Initial Commit

---
+++ SurprizeMeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurprizeMeTest
+//
+//  Created by Gamid on 09.06.2020.
+//  Copyright © 2020 Gamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
